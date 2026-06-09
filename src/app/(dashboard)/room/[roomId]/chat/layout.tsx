3d1e22647fe684bfc1b1e535245c6b09a3b8259a
Salsabilla Regita Anjani <redpainted4@gmail.com>
2026-06-09 15:30:15 +0700

    Bug Fix Grup Chat

--- src/app/(dashboard)/room/[roomId]/chat/layout.tsx
+++ src/app/(dashboard)/room/[roomId]/chat/layout.tsx
@@ -7,7 +7,7 @@ export default function RoomChatLayout({ children }: ChatLayoutProps) {
   return (
     <div
       data-chat-shell
-      className="flex h-full min-h-0 flex-1 flex-col overflow-hidden"
+      className="flex min-h-0 flex-1 flex-col overflow-hidden"
     >
       {children}
     </div>
